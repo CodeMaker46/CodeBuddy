f5b8df5bb82423097cca616eb69d0d5cc99b5e38
Shikshak Kumar <138814848+CodeMaker46@users.noreply.github.com>
2024-12-26 04:18:10 +0530

    Updated socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,15 +1,13 @@
-// src/utils/socket.js
-import { io } from 'socket.io-client';
 
-// Create socket connection
-const socket = io('http://localhost:4000');  // replace with the correct backend URL if deployed elsewhere
+import { io } from 'socket.io-client';
+const socket = io('https://codebuddy-rwfi.onrender.com'); 
 
 socket.on("connect", () => {
-  // console.log("Socket connected", socket.id);  // This should log the socket ID when the connection is successful
+  
 });
 
 socket.on("disconnect", () => {
-  // console.log("Socket disconnected");
+  
 });
 
 
