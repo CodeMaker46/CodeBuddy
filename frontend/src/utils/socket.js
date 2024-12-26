f354201ab0aa05d96739c88e12aaba4a40d7d741
CodeMaker46 <shikshak013@gmail.com>
2024-12-26 22:47:50 +0530

    deployment changes

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,6 @@
 
 import { io } from 'socket.io-client';
-const socket = io('https://codebuddy-rwfi.onrender.com'); // for deployment
+const socket = io('https://codebuddy-backend-uxv9.onrender.com'); // for deployment
 // const socket = io('http://localhost:4000'); // for localhost
 
 socket.on("connect", () => {
